25f4de14e66e3ab8b7b6a93ac5b76637a3bf8a1b
Brennon <brennonyoo@gmail.com>
2026-02-15 12:25:36 -0800

    Motor test second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,75 @@
 #include <Arduino.h>
+#include <IntervalTimer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// pin definitions
+constexpr uint8_t STEP_N_PIN = 2;
+constexpr uint8_t DIR_N_PIN  = 3;
+constexpr uint8_t EN_N_PIN   = 4;
+
+constexpr uint8_t DM_OFF = HIGH;
+constexpr uint8_t DM_ON  = LOW;
+
+IntervalTimer stepTimer;
+
+constexpr uint32_t STEP_PULSE_LOW_US = 5;
+
+void stepISR() {
+
+  digitalWriteFast(STEP_N_PIN, DM_ON);
+  delayMicroseconds(STEP_PULSE_LOW_US);
+  digitalWriteFast(STEP_N_PIN, DM_OFF);
+
+}
+
+void setSpeedSPS(float sps) {
+  if (sps <= 0) {
+    stepTimer.end();
+    digitalWriteFast(STEP_N_PIN, DM_OFF);
+    return;
+
+  }
+
+  float period_us = 1e6f / sps;
+  stepTimer.begin(stepISR, period_us);
+}
+
+void enableDriver(bool enable) {
+  digitalWriteFast(EN_N_PIN, enable ? DM_ON : DM_OFF);
+}
+
+void setDirection(bool forward) {
+  digitalWriteFast(DIR_N_PIN, forward ? DM_ON : DM_OFF );
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(STEP_N_PIN, OUTPUT);
+  pinMode(DIR_N_PIN, OUTPUT);
+  pinMode(EN_N_PIN, OUTPUT);
+
+  digitalWriteFast(STEP_N_PIN, DM_OFF);
+  digitalWriteFast(DIR_N_PIN, DM_OFF);
+  digitalWriteFast(EN_N_PIN, DM_OFF);
+
+  Serial.begin(115200);
+  delay(500);
+
+  enableDriver(true);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  setDirection(true);
+  setSpeedSPS(400);
+  delay(3000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  setSpeedSPS(0);
+  delay(1000);
+
+  setDirection(false);
+  setSpeedSPS(400);
+  delay(3000);
+
+  setSpeedSPS(0);
+  delay(2000);
+  
 }
